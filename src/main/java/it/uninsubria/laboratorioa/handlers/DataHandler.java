d4c984cb069384477addd6a09b13379042c5b7e2
lcavagnari <luca.cavagnari03@proton.me>
2025-07-06 23:40:01 +0200

    feat(load): added load functionality

--- src/main/java/it/uninsubria/laboratorioa/handlers/DataHandler.java
+++ src/main/java/it/uninsubria/laboratorioa/handlers/DataHandler.java
@@ -2,57 +2,128 @@ package it.uninsubria.laboratorioa.handlers;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import it.uninsubria.laboratorioa.objects.Location;
+import it.uninsubria.laboratorioa.objects.Restaurant;
+import it.uninsubria.laboratorioa.objects.enums.Nation;
+import it.uninsubria.laboratorioa.objects.users.User;
 import it.uninsubria.laboratorioa.utils.Constants;
+import lombok.Getter;
+import lombok.experimental.UtilityClass;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
+@UtilityClass
 public class DataHandler {
 
     private static final File ROOT = Constants.ROOT;
-    private static final File COMPANIES_ROOT = new File(ROOT, "companies");
+    private static final File RESTAURANTS_ROOT = new File(ROOT, "companies");
     private static final File USERS_ROOT = new File(ROOT, "users");
 
+    @Getter
+    private final static Map<UUID, Restaurant> restaurantsById = new HashMap<>();
+    @Getter
+    private final static Map<String, Restaurant> restaurantsByName = new HashMap<>();
+    @Getter
+    private final static Map<UUID, Restaurant> restaurantsByOwnerId = new HashMap<>();
 
-    public void loadFromFile() {
 
-        File[] companies;
-        File[] users;
-        try {
-            if (!ROOT.exists()) return;
+    @Getter
+    private final static Map<UUID, User> usersById = new HashMap<>();
+    @Getter
+    private final static Map<String, User> usersByName = new HashMap<>();
 
-            companies = COMPANIES_ROOT.listFiles();
-            users = USERS_ROOT.listFiles();
 
-        } catch (SecurityException ex) {
-            System.out.println("Access denied");
-            return;
-        }
+    private void loadRestaurants(File[] rFiles) {
+        if (rFiles == null) return;
 
-        for (File f : companies) {
+        int count = 0;
+        for (File f : rFiles) {
             try {
                 ObjectMapper mapper = new ObjectMapper();
                 JsonNode jsonNode = mapper.readTree(f);
 
-                /*
-                Restaurant restaurant = new Restaurant(
-                        UUID.fromString(jsonNode.get("id").asText()),
-                        jsonNode.get("name").asText(),
-                        jsonNode.get("address").asText(),
-                        jsonNode.get("phone").asText(),
-                        jsonNode.get("email").asText(),
-                        jsonNode.get("foundationYear").asInt(0)
-                );
 
-                System.out.println(jsonNode.toPrettyString());
-                System.out.println(restaurant);
+                count++;
+                return;
+
+            } catch (IOException e) {
+                System.out.println("ERROR while parsing " + f.getName() + ", cause:" + e.getMessage());
+                return;
+            } catch (SecurityException e) {
+                System.out.println("Access is denied to " + f.getName());
+                return;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    private void loadUsers(File[] uFiles) {
+        if (uFiles == null) return;
+
+        final ObjectMapper mapper = new ObjectMapper();
+        for (File f : uFiles) {
+            try {
+
+                JsonNode jsonNode = mapper.readTree(f);
+                if (jsonNode == null) continue;
+
+                String userName = jsonNode.asText("");
+                String firstName = jsonNode.asText("");
+                String lastName = jsonNode.asText("");
+                //LocalDate dateOfBirth = LocalDate.parse()
+
+                Location loc;
+                try {
+                    loc = new Location(
+                            Nation.valueOf(jsonNode.get("nation").asText()),
+                            jsonNode.get("city").asText(),
+                            jsonNode.get("latitude").asDouble(),
+                            jsonNode.get("longitude").asDouble(),
+                            jsonNode.get("address").asText()
+                    );
+                } catch (Exception ignored) {
+                }
+
+
+                /*
+                User user;
+                if (Objects.equals(jsonNode.get("role").asText(""), "Owner"))
+                    user = new Owner();
+
+                else user = new Client();
+
 
                  */
+
             } catch (IOException e) {
                 System.out.println("ERROR while parsing " + f.getName() + ", cause:" + e.getMessage());
             } catch (SecurityException e) {
                 System.out.println("Access is denied to " + f.getName());
             }
+        }
+    }
+
+
+    public void loadFromFile() {
+
+        File[] restaurants;
+        File[] users;
+        try {
+            if (!ROOT.exists()) return;
+
+            restaurants = RESTAURANTS_ROOT.listFiles();
+            users = USERS_ROOT.listFiles();
+
+            loadUsers(users);
+            loadRestaurants(restaurants);
+
+        } catch (SecurityException ex) {
+            System.out.println("Access denied");
         }
     }
 
---
+++ src/test/java/LoadTest.java
@@ -0,0 +1,7 @@
+import it.uninsubria.laboratorioa.handlers.DataHandler;
+
+public class LoadTest {
+    public static void main(String[] args) {
+        DataHandler.loadFromFile();
+    }
+}
