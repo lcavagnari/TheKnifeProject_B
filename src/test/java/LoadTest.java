f7321dca261962f02e642ea5bb0e8df00b237d3c
lcavagnari <luca.cavagnari03@proton.me>
2025-10-03 22:17:19 +0200

    ref: aggiunto calcolo ms per completare caricamento dati

--- src/test/java/LoadTest.java
+++ src/test/java/LoadTest.java
@@ -2,6 +2,11 @@ import it.uninsubria.laboratorioa.utils.Loader;
 
 public class LoadTest {
     public static void main(String[] args) {
+
+        long timestamp = System.currentTimeMillis();
+
         Loader.loadFromFile();
+
+        System.out.println(System.currentTimeMillis() - timestamp);
     }
 }
