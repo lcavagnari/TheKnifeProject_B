b04002c155be952c4567d3bba71f4a1586ad24bd
semebellissimo <semebellissimo05@gmail.com>
2026-08-27 14:04:59 +0200

    fix(api,rmi): align RMI interfaces and implementations

--- common-api/src/main/java/it/uninsubria/laboratoriob/api/remote/RestaurantServiceInter.java
+++ common-api/src/main/java/it/uninsubria/laboratoriob/api/remote/RestaurantServiceInter.java
@@ -6,14 +6,13 @@ import it.uninsubria.laboratoriob.api.objects.Restaurant;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
 public interface RestaurantServiceInter extends Remote {
     public List<Restaurant> findAll(int offset, int limit) throws RemoteException;
     public long count() throws RemoteException;
-    public Optional<Restaurant> findById(UUID id) throws RemoteException;
+    public Restaurant findById(UUID id) throws RemoteException;
     public List<Restaurant> findByOwner(UUID id) throws  RemoteException;
     public boolean save (Restaurant restaurant) throws RemoteException;
     public boolean update(Restaurant restaurant) throws RemoteException;
--- common-api/src/main/java/it/uninsubria/laboratoriob/api/remote/ReviewServiceInter.java
+++ common-api/src/main/java/it/uninsubria/laboratoriob/api/remote/ReviewServiceInter.java
@@ -9,7 +9,9 @@ import java.util.UUID;
 
 public interface ReviewServiceInter extends Remote {
     public List<Review> findByRestaurant(UUID restaurantId) throws RemoteException;
+    public List<Review> findByUser(UUID userId) throws RemoteException;
     public List<Review> findAll() throws RemoteException;
+    public List<Review> findAll(int offset, int limit) throws RemoteException;
     public boolean save(Review review) throws RemoteException;
     public boolean update(Review review) throws RemoteException;
     public boolean delete(UUID id) throws RemoteException;
